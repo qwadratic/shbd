60e9e523e7a118641f8afc2183151707a72b635a
i <i@is-MacBook-Air.home>
2024-11-07 16:44:22 +0100

    fix localhost

--- webhook.py
+++ webhook.py
@@ -60,4 +60,4 @@ async def payment_webhook(request: Request):
     return {"status": "success"}
 
 if __name__ == "__main__":
-    uvicorn.run(fastapi_app, host="0.0.0.0", port=8000) 
+    uvicorn.run(fastapi_app, host="127.0.0.1", port=8000) 
